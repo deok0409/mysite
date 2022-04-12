6d008b3a083bd4d170d9c2ce4c48f0ffe8dfa87a
deok0409 <k01048303600@gmail.com>
2022-04-12 09:10:36 +0900

    models.py, urls.py 수정

--- pybo/models.py
+++ pybo/models.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
-    modify_date = models.DateTimeField(null=True, blank=True)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    votor = models.ManyToManyField(User)  # voter추가
 
     def __str__(self):
         return self.subject
